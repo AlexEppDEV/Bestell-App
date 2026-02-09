f29e661424215fe4df78905f051c28a40a81e0cb
AlexE.Dev <locha8472@t-online.de>
2026-02-10 00:42:16 +0100

    New dishes added to the array, along with matching title images.

--- Basic code/scripts/foodMenu.js
+++ Basic code/scripts/foodMenu.js
@@ -38,11 +38,28 @@ let foodMenu = [
         "categoriesImg":"Pizza",
         "menu": [
             {
-                "name": "Veggie mushroom black burger",
-                "ingredients": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
-                "menuImg": "Veggie_mushroom_black_burger",
+                "name": "Pizza Margherita",
+                "ingredients": "Tomato Sauce, Mozzarella",
+                "menuImg": "pizza_margherita",
+                "price": "11,90",
+            },
+            {
+                "name": "Pizza Chorizo",
+                "ingredients": "Tomato slices, Mozzarella, Chorizo",
+                "menuImg": "pizza_chorizo",
                 "price": "13,90",
-                
+            },
+            {
+                "name": "Funghi",
+                "ingredients": "Red onion, Olives, Button Mushrooms, Mozzarella",
+                "menuImg": "funghi",
+                "price": "12,90",
+            },
+            {
+                "name": "Quattro Formaggi with Chicken",
+                "ingredients": "Tomato Sauce, Mozzarella",
+                "menuImg": "quattro_formaggi_with_chicken",
+                "price": "15,90",
             },
         ]
 
@@ -52,10 +69,31 @@ let foodMenu = [
         "categoriesImg":"Salad",
         "menu": [
             {
-                "name": "Veggie mushroom black burger",
-                "ingredients": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
-                "menuImg": "Veggie_mushroom_black_burger",
-                "price": "13,90",
+                "name": "Warm beef arugula salad",
+                "ingredients": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+                "menuImg": "warm_beef_arugula_salad",
+                "price": "16,90",
+                
+            },
+            {
+                "name": "Mini green Salad",
+                "ingredients": "Green salad, Cucumber, Carrots, Parsley, Radishes",
+                "menuImg": "mini_green_salad",
+                "price": "7,90",
+                
+            },
+            {
+                "name": "Green Salad with sea food",
+                "ingredients": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+                "menuImg": "green_salad_with_sea_food",
+                "price": "16,90",
+                
+            },
+            {
+                "name": "Vegan green salad with tofu",
+                "ingredients": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+                "menuImg": "vegan_green_salad_with_tofu",
+                "price": "14,90",
                 
             },
         ]
