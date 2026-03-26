4fe487b5c4028400af946f81dfab65dafb6e1d05
AlexE.Dev <locha8472@t-online.de>
2026-03-27 00:56:15 +0100

    Image file sizes adjusted and an error in the naming array fixed.

--- Basic code/scripts/foodMenu.js
+++ Basic code/scripts/foodMenu.js
@@ -8,28 +8,24 @@ let foodMenu = [
                 "ingredients": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
                 "menuImg": "Veggie_mushroom_black_burger",
                 "price": "13,90",
-                
             },
             {
                 "name": "All meat burger",
                 "ingredients": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
                 "menuImg": "All_meat_burger",
                 "price": "15,90",
-                
             },
             {
                 "name": "Beef red burger",
                 "ingredients": "Beef, Cheese, Tomatoes, Lettuce, Onion",
                 "menuImg": "Beef_red_burger",
                 "price": "14,90",
-                
             },
             {
-                "name": "BIg chicken burger",
+                "name": "Big chicken burger",
                 "ingredients": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper, ",
-                "menuImg": "BIg_chicken_burger",
-                "price": "15,90",
-                
+                "menuImg": "Big_chicken_burger",
+                "price": "15,90", 
             },
         ]
     },
@@ -62,7 +58,6 @@ let foodMenu = [
                 "price": "15,90",
             },
         ]
-
     },
     {
         "categories":"Salad",
@@ -73,31 +68,26 @@ let foodMenu = [
                 "ingredients": "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
                 "menuImg": "warm_beef_arugula_salad",
                 "price": "16,90",
-                
             },
             {
                 "name": "Mini green Salad",
                 "ingredients": "Green salad, Cucumber, Carrots, Parsley, Radishes",
                 "menuImg": "mini_green_salad",
                 "price": "7,90",
-                
             },
             {
                 "name": "Green Salad with sea food",
                 "ingredients": "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
                 "menuImg": "green_salad_with_sea_food",
                 "price": "16,90",
-                
             },
             {
                 "name": "Vegan green salad with tofu",
                 "ingredients": "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
                 "menuImg": "vegan_green_salad_with_tofu",
                 "price": "14,90",
-                
             },
         ]
-
     },
 ];
 
